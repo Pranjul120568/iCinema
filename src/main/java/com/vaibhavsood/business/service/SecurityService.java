c853a92c093ade8571abf5ab04842456cf299b26
Pranjul120568 <pranjuldwivedi120568@gmail.com>
2024-10-04 15:08:45 +0530

    Removed unnecesary files

--- src/main/java/com/vaibhavsood/business/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.vaibhavsood.business.service;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autologin(String username, String password);
-}
--- src/main/java/com/vaibhavsood/business/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.vaibhavsood.business.service;
-
-import com.vaibhavsood.data.entity.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
